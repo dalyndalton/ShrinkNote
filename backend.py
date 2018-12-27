dc6f29afad6d2903cafadce3f094147dc872e505
dalyndalton <dalyn.dalton.dd@gmail.com>
2018-12-27 11:03:46 -0700

    Periodic Dev Update

--- backend.py
+++ backend.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import filedialog as fd
 
 import numpy as np
+from scipy.cluster.vq import kmeans, vq
 from PIL import Image
 
 
@@ -34,41 +35,6 @@ def bit_depth(array, bits=4):
     return ((array.astype(np.uint8) >> shift) << shift) + half
 
 
-def sample(array, percent=.10):
-    """Samples the percent of the image specified, returns bg color"""
-    array = array.reshape((-1, 3))
-    amount = int(float(array.shape[0]) * percent)  # gets the length of the array, then takes the sample fraction
-    index = np.arange(amount)  # same as list(range(...)), but more condense
-    np.random.shuffle(index)
-    subset = array[index[:amount]]
-    packed = rgb_packer(bit_depth(subset).astype(int))
-    unique, counts = np.unique(packed, return_counts=True)
-
-    return rgb_packer(unique[counts.argmax()], pack=False)
-
-
-class Notes:
-
-    def __init__(self, hsv_bg_color=None):
-        self.image = open_image()
-        if hsv_bg_color is None:
-            self.bg_color = rgb_to_hsv(sample(img[0], .5))
-        else:
-            self.bg_color = hsv_bg_color
-        self.palette = self.get_palette()
-
-    def threshold(self):
-        for x in self.image:
-            for y in self.image[1]:
-
-    def get_palette(self):
-        _, sat, val = self.bg_color
-
-
-
-
-
-
 def open_image():
     """Function used to test filetype and open image, converts file to RBG"""
     tk.Tk().withdraw()
@@ -83,6 +49,7 @@ def open_image():
             break
         else:
             print("ERROR: NO FILE SELECTED")
+
     if img.mode != 'RGB':
         img = img.convert('RGB')
 
@@ -106,7 +73,62 @@ def rgb_packer(array, pack=True):
         return np.hstack(rgb).reshape(orig_shape + (3,))
 
 
-img = open_image()
-background_rgb = sample(img[0], .5)
-print(*background_rgb)
-print(rgb_to_hsv(*background_rgb))
+class Notes:
+
+    def __init__(self, bg_rgb=None, v_thresh=30, s_thresh=20, bitdepth=6, colorcount=7, palette=None):
+        self.image_rgb, self.image_hsv = open_image()
+        self.image_final = Image
+        self.bit_depth = bitdepth
+        self.color_count = colorcount - 1
+
+        if bg_rgb is None:
+            self.bg_color_rbg = self.sample(self.image_rgb, .5)
+            self.bg_color_hsv = rgb_to_hsv(*self.bg_color_rbg)
+        else:
+            self.bg_color_rbg = bg_rgb
+            self.bg_color_hsv = rgb_to_hsv(*bg_rgb)
+
+        self.v_threshold = v_thresh
+        self.s_threshold = s_thresh
+
+        self.color_palette = palette
+
+    def threshold(self):
+        """Determines foreground and background colors, and applies color palette"""
+        _, s_bg, v_bg = self.bg_color_hsv
+        s_pix = self.image_hsv[:, :, 1]
+        v_pix = self.image_hsv[:, :, 2]
+        s_diff = np.abs(s_bg - s_pix)
+        v_diff = np.abs(v_bg - v_pix)
+        foreground = ((v_diff >= self.v_threshold) | (s_diff >= self.s_threshold))
+        colors = kmeans(self.image_hsv[foreground].astype(np.float32),
+                        self.color_count)  # Thank you fancy SciPy clusters
+        self.color_palette = np.vstack((self.bg_color_hsv, colors))
+        foreground = foreground.flatten()
+        temp = self.image_hsv.reshape((-1, 3))
+        palette = np.zeros(temp.shape[0], dtype=np.uint8)
+        palette = vq(temp[foreground], palette)  # returns codes and distance, only need codes
+        palette = palette.reshape(self.image_hsv.shape[:-1])
+
+        return palette
+
+    def sample(self, array, percent=.10):
+        """Samples the percent of the image specified, returns bg color"""
+        array = array.reshape((-1, 3))
+        amount = int(float(array.shape[0]) * percent)  # gets the length of the array, then takes the sample fraction
+        index = np.arange(amount)  # same as list(range(...)), but more condense
+        np.random.shuffle(index)
+        subset = array[index[:amount]]
+        # self.sample_set = subset  #  In the case that a sample set needs to be defined
+        packed = rgb_packer(bit_depth(subset, self.bit_depth).astype(int))
+        unique, counts = np.unique(packed, return_counts=True)
+
+        return rgb_packer(unique[counts.argmax()], pack=False)
+
+    def process(self):
+        self.threshold()
+
+
+
+f = Notes()
+f.process()
