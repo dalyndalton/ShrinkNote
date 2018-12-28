c2129ba41d998a25b88ad06c79b4cc2d24769bae
dalyndalton <dalyn.dalton.dd@gmail.com>
2018-12-27 21:56:00 -0700

    Periodic Dev Update

--- backend.py
+++ backend.py
@@ -1,9 +1,12 @@
 import tkinter as tk
 from tkinter import filedialog as fd
+import math
 
 import numpy as np
 from scipy.cluster.vq import kmeans, vq
 from PIL import Image
+
+np.set_printoptions(threshold=np.nan)
 
 
 def rgb_to_hsv(r, g, b):
@@ -28,6 +31,34 @@ def rgb_to_hsv(r, g, b):
     return h, s, v
 
 
+def hsv_to_rgb(h, s, v):
+    h = float(h)
+    s = float(s) / 100
+    v = float(v) / 100
+    h60 = h / 60.0
+    h60f = math.floor(h60)
+    hi = int(h60f) % 6
+    f = h60 - h60f
+    p = v * (1 - s)
+    q = v * (1 - f * s)
+    t = v * (1 - (1 - f) * s)
+    r, g, b = 0, 0, 0
+    if hi == 0:
+        r, g, b = v, t, p
+    elif hi == 1:
+        r, g, b = q, v, p
+    elif hi == 2:
+        r, g, b = p, v, t
+    elif hi == 3:
+        r, g, b = p, q, v
+    elif hi == 4:
+        r, g, b = t, p, v
+    elif hi == 5:
+        r, g, b = v, p, q
+    r, g, b = int(r * 255), int(g * 255), int(b * 255)
+    return int(r), int(g), int(b)
+
+
 def bit_depth(array, bits=4):
     """Compresses an image's color palette by zeroing out significant bits"""
     shift = 8 - bits
@@ -35,15 +66,13 @@ def bit_depth(array, bits=4):
     return ((array.astype(np.uint8) >> shift) << shift) + half
 
 
-def open_image():
+def open_image(file_path):
     """Function used to test filetype and open image, converts file to RBG"""
     tk.Tk().withdraw()
     while True:
-        file = fd.askopenfilename(title="Select Image", initialdir="C:/Users/%username%/Documents",
-                                  filetypes=(("Images", ".jpeg"), ("Images", ".jpg"), ("Images", ".png")))
-        if file != "":
+        if file_path != "":
             try:
-                img = Image.open(file)
+                img = Image.open(file_path)
             except IOError:
                 print("ERROR: INVALID FILE TYPE")
             break
@@ -54,9 +83,7 @@ def open_image():
         img = img.convert('RGB')
 
     rgb = np.array(img)
-    hsv = np.array(img.convert('HSV'))
-
-    return rgb, hsv
+    return rgb
 
 
 def rgb_packer(array, pack=True):
@@ -73,62 +100,117 @@ def rgb_packer(array, pack=True):
         return np.hstack(rgb).reshape(orig_shape + (3,))
 
 
+def sample(array, percent=10):
+    """Samples the percent of the image specified, returns bg color"""
+    array = array.reshape((-1, 3))
+    amount = int(
+        float(array.shape[0]) * float(percent))  # gets the length of the array, then takes the sample fraction
+    index = np.arange(array.shape[0])  # same as list(range(...)), but more condense
+    np.random.shuffle(index)
+    subset = array[index[:amount]]
+
+    return subset
+
+
 class Notes:
 
-    def __init__(self, bg_rgb=None, v_thresh=30, s_thresh=20, bitdepth=6, colorcount=7, palette=None):
-        self.image_rgb, self.image_hsv = open_image()
+    def __init__(self, filepath, bg_rgb=None, v_thresh=30, s_thresh=20, bitdepth=6, colorcount=7, palette=None):
+
+        self.file_path = filepath
+        self.image_rgb = open_image(filepath)
+
+        self.image_hsv = self.image_rgb
+        for x in range(0, self.image_rgb.shape[0]):
+            for y in range(0, self.image_rgb.shape[1]):
+                self.image_hsv[x, y] = rgb_to_hsv(*self.image_rgb[x, y])
+        self.image_rgb = (open_image(filepath))  # reassigns image_rgb, doesnt work without it
+
         self.image_final = Image
         self.bit_depth = bitdepth
-        self.color_count = colorcount - 1
+        self.color_count = colorcount
 
-        if bg_rgb is None:
-            self.bg_color_rbg = self.sample(self.image_rgb, .5)
-            self.bg_color_hsv = rgb_to_hsv(*self.bg_color_rbg)
+        self.bg_color_rgb = self.get_bg_color(self.image_rgb)
+        self.bg_color_hsv = rgb_to_hsv(*self.bg_color_rgb)
+
+        if bg_rgb is not None:
+            self.custom_bg = bg_rgb
+
         else:
-            self.bg_color_rbg = bg_rgb
-            self.bg_color_hsv = rgb_to_hsv(*bg_rgb)
+            self.custom_bg = None
 
         self.v_threshold = v_thresh
         self.s_threshold = s_thresh
 
         self.color_palette = palette
+        self.sample_set = []
+
+        self.process()
+
+    def foreground(self, sample_size):
+
+        samp = sample_size
+
+        _, s_bg, v_bg = self.bg_color_hsv
+        s_pix = samp[:, 1]
+        v_pix = samp[:, 2]
+        s_diff = np.abs(s_bg - s_pix)
+        v_diff = np.abs(v_bg - v_pix)
+        return (v_diff >= self.v_threshold) | (s_diff >= self.s_threshold), samp
 
     def threshold(self):
         """Determines foreground and background colors, and applies color palette"""
-        _, s_bg, v_bg = self.bg_color_hsv
-        s_pix = self.image_hsv[:, :, 1]
-        v_pix = self.image_hsv[:, :, 2]
-        s_diff = np.abs(s_bg - s_pix)
-        v_diff = np.abs(v_bg - v_pix)
-        foreground = ((v_diff >= self.v_threshold) | (s_diff >= self.s_threshold))
-        colors = kmeans(self.image_hsv[foreground].astype(np.float32),
-                        self.color_count)  # Thank you fancy SciPy clusters
-        self.color_palette = np.vstack((self.bg_color_hsv, colors))
-        foreground = foreground.flatten()
-        temp = self.image_hsv.reshape((-1, 3))
-        palette = np.zeros(temp.shape[0], dtype=np.uint8)
-        palette = vq(temp[foreground], palette)  # returns codes and distance, only need codes
-        palette = palette.reshape(self.image_hsv.shape[:-1])
+        foreground, samp = self.foreground(sample(self.image_hsv))
+
+        colors, _ = kmeans(samp[foreground].astype(np.float32),
+                           self.color_count - 1)  # Thank you fancy SciPy clusters
+
+        # Convert colors back to rgb
+        for x in range(colors.shape[0]):
+            colors[x] = hsv_to_rgb(*colors[x])
+
+        self.color_palette = np.vstack((self.bg_color_rgb, colors)).astype(np.uint8)
+
+        mask, _ = self.foreground(self.image_hsv.reshape((-1, 3)))
+
+        pix = self.image_rgb.reshape((-1, 3))
+        mask = mask.flatten()
+        labs = np.zeros(pix.shape[0], dtype=np.uint8)
+        labs[mask], _ = vq(pix[mask], self.color_palette)  # returns codes and distance, only need codes
+        palette = labs.reshape(self.image_hsv.shape[:-1])
+        print(labs, "\n \n \n", palette)
 
         return palette
 
-    def sample(self, array, percent=.10):
-        """Samples the percent of the image specified, returns bg color"""
-        array = array.reshape((-1, 3))
-        amount = int(float(array.shape[0]) * percent)  # gets the length of the array, then takes the sample fraction
-        index = np.arange(amount)  # same as list(range(...)), but more condense
-        np.random.shuffle(index)
-        subset = array[index[:amount]]
-        # self.sample_set = subset  #  In the case that a sample set needs to be defined
-        packed = rgb_packer(bit_depth(subset, self.bit_depth).astype(int))
-        unique, counts = np.unique(packed, return_counts=True)
+    def get_bg_color(self, array, percent=None):
+        if percent is not None:
+            subset = sample(array, percent)
+        else:
+            subset = sample(array)
 
+        packed = rgb_packer(bit_depth(subset, self.bit_depth).astype(np.uint8))
+        unique, counts = np.unique(packed, return_counts=True)
         return rgb_packer(unique[counts.argmax()], pack=False)
 
     def process(self):
-        self.threshold()
+        temp_image = self.threshold()
+        pal = self.color_palette.astype(np.float32)
+        # saturate palette
+        for x in range(pal.shape[0]):
+            pal[x] = rgb_to_hsv(*pal[x])
+            hue, sat, _ = pal[x]
+            pal[x] = hue, sat, 100
+            pal[x] = hsv_to_rgb(*pal[x])
+
+        self.color_palette = pal.astype(np.uint8)
+        if self.custom_bg is not None:
+            self.color_palette[0] = self.custom_bg
+        self.image_final = Image.fromarray(temp_image, 'P')
+        self.image_final.putpalette(self.color_palette.flatten())
 
 
-
-f = Notes()
-f.process()
+tk.Tk().withdraw()
+file_path = file = fd.askopenfilename(title="Select Image", initialdir="C:/Users/%username%/Documents",
+                                      filetypes=(("Images", ".jpeg"), ("Images", ".jpg"), ("Images", ".png")))
+f = Notes(file_path, bitdepth=6, v_thresh=30, s_thresh=20, colorcount=8)
+f.image_final.show()
+input()
